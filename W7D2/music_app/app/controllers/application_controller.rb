6c9e0247783e1714fa985bc4e0835878db424d39
Will Offit <will.offit@gmail.com>
2020-07-14 16:59:06 -0700

    W7D2

---
+++ W7D2/music_app/app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login_user!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+end
---
+++ W7D2/music_app/app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def show
+    render :show
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+  
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login_user!(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    self.params.require(:user).permit(:email)
+  end
+end
